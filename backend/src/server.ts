5f7563962aef4701ba8feb4f7e5d623d815f50c2
Mishi <mishi.beraia@gmail.com>
2026-05-29 10:19:08 +0400

    added health endpoint for backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+
+app.get("/api/health", (req, res) => {
+    res.json({ ok: true});
+});
+
+app.listen(3000, () => {
+    console.log("Backend running on http://localhost:3000");
+});
